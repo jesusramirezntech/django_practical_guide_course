df30efd8b26562c097dcd5921beb5d50545be043
JesusRamirezN <jesshack10@gmail.com>
2023-07-12 16:34:25 -0600

    Returning response with dynamic and not harcoded behavior

---
+++ myproject/challenges/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    #If the request reaches /january, then execute views.index
+    #path("january", views.january),
+    #place holder syntax for django
+    #str: means tells django should be trated as string
+    #int: would treat the value as int
+    
+    #THe third parameter is the label name that the path would be reference with
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
+]
--- myproject/challenges/views.py
+++ myproject/challenges/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
+monthly_challenges = {
+    "january" : "This is january",
+    "february" : "This is february",
+    "march" : "This is march",
+    "april" : "This is april",
+    "may" : "This is may",
+    "june" : "This is june",
+    "july" : "This is july",
+    "august" : "This is august",
+    "september" : "This is september",
+    "october" : "This is october",
+    "november" : "This is november",
+    "december" : "This is december",
+}
 # Create your views here.
+"""
+def monthly_redirect(request,month):
+    return HttpResponseRedirect("/challenges/" + somePath)
+"""
+
+
+#Using labeled URLs
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month index")
+    
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+#month SHOULD BE the same name defined in the URLs file path
+def monthly_challenge(request, month):
+    
+    try:
+        return HttpResponse(monthly_challenges[month])
+    except:
+        return HttpResponseNotFound("This path is not supported")
+
